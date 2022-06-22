c0e5d2f23e86fb7f9a19ed71b63e76eac61619d5
WinsonChen114 <14winsonchen@gmail.com>
2022-06-22 13:25:09 -0400

    Added initial Routes and variables

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -2,12 +2,34 @@ import * as React from "react"
 import Navbar from "../Navbar/Navbar"
 import Home from "../Home/Home"
 import "./App.css"
+import { BrowserRouter, Routes, Route, } from "react-router-dom"
+import TransactionDetail from "../TransactionDetail/TransactionDetail"
 
 export default function App() {
+
+  //Whether or not the app is currently requesting data from the API
+  const [isLoading, setIsLoading] = React.useState()
+  //List of bank transaction items fetched from the API
+  const [transactions, setTransactions] = React.useState()
+  //List of bank transfer items fetched from the API
+  const [transfers, setTransfers] = React.useState()
+  //Errors associated with fetching data from the API
+  const [error, setError] = React.useState()
+  //String value used to create a controlled input in the FilterInput.jsx component
+  const [filterInputValue, setFilterInputValue] = React.useState()
+
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
+    <div className="app">
+      <BrowserRouter>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/transactions/:transactionId" element={<TransactionDetail />} />
+          </Routes>
+
+        </main>
+      </BrowserRouter>
     </div>
   )
 }
